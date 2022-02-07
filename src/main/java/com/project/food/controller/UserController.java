8be61af37b97ce8c303dfb2e3b7a46f71c237105
riazi <amirabbas.riazi@gmail.com>
2022-02-07 23:55:17 +0330

    fix: memberService Injection in UserController

--- src/main/java/com/project/food/controller/UserController.java
+++ src/main/java/com/project/food/controller/UserController.java
@@ -2,17 +2,20 @@ package com.project.food.controller;
 
 import com.project.food.domain.Member;
 import com.project.food.service.MemberService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private MemberService memberService;
+
+    public UserController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
 
     @PostMapping("/add")
     public ResponseEntity<Object> addUser(@RequestBody Member member) {
